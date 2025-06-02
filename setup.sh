296ea399b351aae6b90442aea6ff4738739c4e65
Ahmad Imran <ahmadimran@>
2025-06-02 15:51:25 +0500

    intial commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+git init
+git branch -m develop
+git config --local user.name "Ahmad Imran"
+git config --local user.email "ahmadimran@"
+git config --local core.editor notepad
+mkdir ./src ./styles ./assests ./bin ./nodemodules
+touch ./src/index.html ./styles/index.css ./.gitignore
+git remote add origin https://github.com/ahmad2912-cpu/s1week11.git
+
+echo "new repo setup"
